7b5da4750dccdc646b0742e17b645486f00aedf5
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-02-15 16:02:13 -0500

    fix datefield

--- src/app/customers/forms.py
+++ src/app/customers/forms.py
@@ -1,18 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, DateField, SubmitField
+from wtforms import StringField, SubmitField
+from wtforms.fields.html5 import DateField
 from wtforms.validators import DataRequired
 from wtforms.sqlalchemy.fields import QuerySelectField
 from app.model import MembershipType
 
+    
+def choice_query():
+    return MembershipType.query
 
 class CustomerForm(FlaskForm):
     first_name = StringField('Enter customer name', validators=[DataRequired()])
     last_name = StringField('Enter customer name', validators=[DataRequired()])
-    date_of_birth = DateField('Enter customer birthdate', validators=[DataRequired()])
-    membership_type = QuerySelectField('Select membership type', query_factory=choice_query, 
+    date_of_birth = DateField('Enter customer birthdate', format='%Y-%m-%d', validators=[DataRequired()])
+    membership_type = QuerySelectField('Select membership type', query_factory=choice_query,
                                        get_label='name', validators=[DataRequired()])
     submit = SubmitField('Submit')
-    
-
-def choice_query():
-    return MembershipType.query
--- src/app/lenses/forms.py
+++ src/app/lenses/forms.py
@@ -1,18 +1,18 @@
-from flask_wtf import FlaskForm 
-from wtforms import StringField, DateField, BooleanField, SelectField, SubmitField
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SelectField, SubmitField
 from wtforms.validators import DataRequired
 from wtforms.sqlalchemy.fields import QuerySelectField
+from wtforms.fields.html5 import DateField
 from app.model import Lens
 
 
-class LensForm(FlaskForm):
-    name = StringField('Enter lens name', validators=[DataRequired()])
-    release_date = DateField('Enter lens release_date', validators=[DataRequired()])
-    stock = IntegerField('Enter lens stock', validators=[DataRequired()])
-    format = QuerySelectField('Select lens format', query_factory=choice_query, 
-                                get_label='name', validators=[DataRequired()])
-    submit = SubmitField('Submit')
-    
-
 def choice_query():
     return Lens.query
+
+class LensForm(FlaskForm):
+    name = StringField('Enter lens name', validators=[DataRequired()])
+    release_date = DateField('Enter lens release_date', format='%Y-%m-%d', validators=[DataRequired()])
+    stock = IntegerField('Enter lens stock', validators=[DataRequired()])
+    format = QuerySelectField('Select lens format', query_factory=choice_query,
+                                get_label='name', validators=[DataRequired()])
+    submit = SubmitField('Submit')
