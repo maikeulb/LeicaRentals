53f854fb93068b12bf25ccb9a4ccd0ac78c9c03d
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-02-15 23:43:35 -0500

    add lenses api

--- src/app/api/__init__.py
+++ src/app/api/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-api  = Blueprint('api', __name__)
+api = Blueprint('api', __name__)
 
-from app.api import views
+from app.api import customers, lenses, rentals
---
+++ src/app/api/lenses.py
@@ -0,0 +1,73 @@
+import sys
+from datetime import datetime
+from flask import (
+    render_template,
+    flash,
+    redirect,
+    url_for,
+    request,
+    jsonify,
+    current_app
+)
+from flask_login import current_user, login_required
+from app.extensions import db
+from app.api import api
+import json
+from app.models import (
+    Lens,
+)
+
+@api.route('/api/lenses/<query>', defaults={'query': None})
+def get_lenses(query):
+    lens_query = Lens.query.filter_by(Lens.stock >0)
+
+    if query:
+        lens_query = lens_query.filter(Lens.name.contains(query))
+    lenses = lens_query.all()
+
+    response = jsonify([lens.to_dict() for lens in lenses])
+    return response
+
+
+@api.route('/api/lenses/<int:id>')
+def get_lens(id):
+    lens = Lens.query.get_or_404(id)
+
+    response = jsonify(lens.to_dict())
+    return response
+
+
+@api.route('/api/lenses/<int:id>', methods=['POST'])
+def create_lens(id):
+    data = request.get_json() or {}
+    if 'first_name' not in data or 'last_name' not in data \
+       return bad_request('must include first_name, last_name')
+
+    lens = Lens()
+    lens.from_dict(data)
+
+    db.session.add(lens)
+    db.session.commit()
+
+    response = jsonify(lens.to_dict())
+    return response
+
+
+@api.route('/api/lenses/<int:id>', methods=['PUT'])
+def update_lens(id):
+    lens = Lens.query.filter_by(id=id).first_or_404()
+    lens.from_dict(request.get_json() or {})
+
+    db.session.commit()
+
+    response = jsonify(lens.to_dict())
+    return response
+
+
+@api.route('/api/lenses/<int:id>', methods=['Delete'])
+def delete_lens(id):
+    Lens.query.filter_by(id=id).delete()
+    db.session.commit()
+
+    response = jsonify({'data': 'success'})
+    return response
