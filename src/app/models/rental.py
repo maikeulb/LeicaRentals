6e19ddcd585eee8472e72b8a0a11a5352a9d77c5
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-02-17 14:20:24 -0500

    add datetime default

--- src/app/models/rental.py
+++ src/app/models/rental.py
@@ -6,7 +6,7 @@ class Rental(db.Model):
     __tablename__ = 'rentals'
 
     id = db.Column(db.Integer, primary_key=True)
-    date_rented = db.Column(db.DateTime)
+    date_rented = db.Column(db.DateTime, default=datetime.utcnow)
     date_returned = db.Column(db.DateTime)
     customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
     lens_id = db.Column(db.Integer, db.ForeignKey('lenses.id'))
